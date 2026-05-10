eee8c94c2eaa7bcc1335120fa2caaaf40b396e7d
d331977151 <d331977151@gmail.com>
2026-05-10 17:07:53 +0800

    Add files via upload

---
+++ survis_0.1.0_20151022_local/src/data/generated/bib.js
@@ -0,0 +1,113 @@
+﻿define({ entries : {
+    "alsaleh2024fewshot": {
+        "author": "Alsaleh, A. M. and Albalawi, E. and Algosaibi, A. and Albakheet, S. S. and Khan, S. B.",
+        "doi": "10.3390/diagnostics14121213",
+        "journal": "Diagnostics",
+        "keywords": "type:method, application:segmentation, method:meta_learning",
+        "number": "12",
+        "pages": "1213",
+        "title": "Few-shot learning for medical image segmentation using 3D U-Net and model-agnostic meta-learning (MAML)",
+        "type": "article",
+        "volume": "14",
+        "year": "2024"
+    },
+    "dissanayake2025review": {
+        "author": "Dissanayake, T. and George, Y. and Mahapatra, D. and Sridharan, S. and Fookes, C. and Ge, Z.",
+        "doi": "10.1145/3746224",
+        "journal": "ACM Computing Surveys",
+        "keywords": "type:survey, application:segmentation, method:review",
+        "number": "1",
+        "title": "Few-shot learning for medical image segmentation: A review and comparative study",
+        "type": "article",
+        "volume": "58",
+        "year": "2025"
+    },
+    "jessen2025efficient": {
+        "author": "Jessen, A. and Blattgerste, C. and Legnar, M. and Popovic, Z. V. and Porubsky, S. and Weis, C.-A.",
+        "doi": "10.1007/s00428-025-04351-8",
+        "journal": "Virchows Archiv",
+        "keywords": "type:method, application:medical_imaging, method:efficient_finetuning",
+        "title": "Efficient finetuning of foundation model combined with few-shot learning improves pattern recognition in histopathology",
+        "type": "article",
+        "year": "2025"
+    },
+    "lan2026oflsam2": {
+        "author": "Lan, M. and Zhang, L. and Li, X.",
+        "booktitle": "Proceedings of the AAAI Conference on Artificial Intelligence",
+        "doi": "10.1609/aaai.v40i7.37502",
+        "keywords": "type:method, application:segmentation, method:SAM2",
+        "number": "7",
+        "pages": "5809--5817",
+        "title": "OFL-SAM2: Prompt SAM2 with online few-shot learner for efficient medical image segmentation",
+        "type": "inproceedings",
+        "volume": "40",
+        "year": "2026"
+    },
+    "ma2024segment": {
+        "author": "Ma, J. and He, Y. and Li, F. and Han, L. and You, C. and Wang, B.",
+        "doi": "10.1038/s41467-024-44824-z",
+        "journal": "Nature Communications",
+        "keywords": "type:method, application:segmentation, method:SAM",
+        "pages": "654",
+        "title": "Segment anything in medical images",
+        "type": "article",
+        "volume": "15",
+        "year": "2024"
+    },
+    "moglia2026generalist": {
+        "author": "Moglia, A. and Leccardi, M. and Cavicchioli, M. and Maccarini, A. and Ferrari, M. and Casiraghi, E. and Ferrante, E.",
+        "doi": "10.1016/j.inffus.2025.103709",
+        "journal": "Information Fusion",
+        "keywords": "type:survey, application:segmentation, method:generalist_model",
+        "pages": "103709",
+        "title": "Generalist models in medical image segmentation: A survey and performance comparison with task-specific approaches",
+        "type": "article",
+        "volume": "127",
+        "year": "2026"
+    },
+    "noh2025foundation": {
+        "author": "Noh, S. and Lee, B.-D.",
+        "doi": "10.21037/qims-2024-2826",
+        "journal": "Quantitative Imaging in Medicine and Surgery",
+        "keywords": "type:survey, application:segmentation, method:foundation_model",
+        "number": "6",
+        "pages": "5825--5858",
+        "title": "A narrative review of foundation models for medical image segmentation: Zero-shot performance evaluation on diverse modalities",
+        "type": "article",
+        "volume": "15",
+        "year": "2025"
+    },
+    "pachetti2024systematic": {
+        "author": "Pachetti, E. and Colantonio, S. and Falcini, F. and others",
+        "doi": "10.1016/j.artmed.2024.102949",
+        "journal": "Artificial Intelligence in Medicine",
+        "keywords": "type:survey, application:medical_imaging, method:review",
+        "pages": "102949",
+        "title": "A systematic review of few-shot learning in medical imaging",
+        "type": "article",
+        "volume": "156",
+        "year": "2024"
+    },
+    "shi2025domain": {
+        "author": "Shi, W. and Zhang, P. and Li, Y. and Jiang, Z.",
+        "doi": "10.1007/s40747-024-01625-7",
+        "journal": "Complex \\& Intelligent Systems",
+        "keywords": "type:method, application:segmentation, method:domain_tuning",
+        "pages": "37",
+        "title": "Segment anything model for few-shot medical image segmentation with domain tuning",
+        "type": "article",
+        "volume": "11",
+        "year": "2025"
+    },
+    "woerner2024navigating": {
+        "author": "Woerner, S. and Baumgartner, C. F.",
+        "booktitle": "Foundation models for general medical AI",
+        "doi": "10.1007/978-3-031-73471-7_4",
+        "keywords": "type:benchmark, application:medical_imaging, method:foundation_model",
+        "pages": "30--39",
+        "publisher": "Springer",
+        "title": "Navigating data scarcity using foundation models: A benchmark of few-shot and zero-shot learning approaches in medical imaging",
+        "type": "incollection",
+        "year": "2024"
+    }
+}});
